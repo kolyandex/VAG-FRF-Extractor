9d4c99fd07bb268bb07141d7e6faf107d7f5b44b
Nickolay Fadeev <iam@kolyandex.su>
2022-05-01 23:03:48 +0700

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace VAG_FRF_Extractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_OpenFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_OpenFile
+            // 
+            this.button_OpenFile.Location = new System.Drawing.Point(12, 12);
+            this.button_OpenFile.Name = "button_OpenFile";
+            this.button_OpenFile.Size = new System.Drawing.Size(163, 84);
+            this.button_OpenFile.TabIndex = 0;
+            this.button_OpenFile.Text = "Open file";
+            this.button_OpenFile.UseVisualStyleBackColor = true;
+            this.button_OpenFile.Click += new System.EventHandler(this.Button_OpenFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(187, 108);
+            this.Controls.Add(this.button_OpenFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "VAG FRF Extractor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_OpenFile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+
+namespace VAG_FRF_Extractor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OpenFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var ofd = new OpenFileDialog();
+                ofd.Filter = "FRF Files |*.frf";
+                if (ofd.ShowDialog() != DialogResult.OK) return;
+                if (File.Exists(ofd.FileName) == false) return;
+                var key = Key.GetKey();
+                var crypted_file = File.ReadAllBytes(ofd.FileName);
+                var decrypted_file = new byte[crypted_file.Length];
+                int first_seed = 0;
+                int second_seed = 1;
+                for (int i = 0; i < crypted_file.Length; i++)
+                {
+                    var key_byte = key[i % key.Length];
+                    first_seed = ((first_seed + key_byte) * 3) & 0xFF;
+                    decrypted_file[i] = (byte)(crypted_file[i] ^ (first_seed ^ 0xFF ^ second_seed ^ key_byte));
+                    second_seed = ((second_seed + 1) * first_seed) & 0xFF;
+                }
+                var fbd = new FolderBrowserDialog();
+                if (fbd.ShowDialog() != DialogResult.OK) return;
+
+                File.WriteAllBytes("decrypted.zip", decrypted_file);
+                ZipFile.ExtractToDirectory("decrypted.zip", fbd.SelectedPath);
+                File.Delete("decrypted.zip");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+    }
+}
